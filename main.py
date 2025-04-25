ec609b7e0a41622a6e51ec5c6a44dfefe396ec89
Akbhar <112480476+AkbharChowdhury@users.noreply.github.com>
2025-04-25 18:49:57 +0100

    a

--- main.py
+++ main.py
@@ -1,20 +1,45 @@
 import requests
 import json
-from pokemon_class import  Pokemon
-base_url: str = 'https://pokeapi.co/api/v2/pokemon/'
+from pokemon_class import Pokemon
+
+base_url: str = 'https://pokeapi.co/api/v2/'
+
+
+def get_pokemon_hint(limit: int):
+    '''
+
+    :param limit: int
+    :return: a list of suggested pokemons
+    '''
+    url = f'{base_url}pokemon?limit={limit}&offset=2'
+    request = requests.get(url)
+    data = request.json()
+    suggestions = data['results']
+    return (suggestion['name'] for suggestion in suggestions)
+
+
 def get_pokemon(name: str):
-    pokemon = requests.get(f'{base_url}/{name}')
+    pokemon = requests.get(f'{base_url}/pokemon/{name}')
     if pokemon.status_code == 404:
         print('this pokemon cannot be found!')
         return dict()
     return pokemon.json()
 
 
-def main():
+def request_pokemon():
     pokemon_name: str = input('enter pokemon: '.title()).strip()
-
-
     pokemon = get_pokemon(pokemon_name)
+    return pokemon
+
+
+def main():
+    suggested_pokemons = list(get_pokemon_hint(2))
+
+    pokemon = request_pokemon()
+    while not pokemon:
+        print('suggestions'.capitalize())
+        print(suggested_pokemons)
+        pokemon = request_pokemon()
 
     if pokemon:
         # print(json.dumps(pokemon, indent=2))
@@ -24,9 +49,7 @@ def main():
         print(f'weight: {p.weight}'.title())
         print('abilities'.capitalize())
         for ability in p.abilities:
-            print(ability['ability']['name'], 'slots',ability['slot'])
-
-
+            print(ability['ability']['name'], 'slots', ability['slot'])
 
 
 if __name__ == '__main__':
